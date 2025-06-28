5c0de3b1e43a4df90511fa32ccb250fd2aada55f
Bello Yusuf Alani <92359313+yusufbello770@users.noreply.github.com>
2025-06-28 22:27:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -266,3 +266,5 @@ if __name__ == '__main__':
     app.run(debug=True)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
